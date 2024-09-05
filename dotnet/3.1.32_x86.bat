f5f7beabd3ac16fae22533076d33fe1b8ce78c7c
雪リリ <yukinshell@qq.com>
2024-09-05 21:36:02 +0800

    分离32 64

---
+++ dotnet/3.1.32_x86.bat
@@ -0,0 +1,2 @@
+curl -OL https://download.visualstudio.microsoft.com/download/pr/3f353d2c-0431-48c5-bdf6-fbbe8f901bb5/542a4af07c1df5136a98a1c2df6f3d62/windowsdesktop-runtime-3.1.32-win-x86.exe
+pause
